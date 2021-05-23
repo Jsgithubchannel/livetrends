b49512a2520360d586e816ab05cc8055ad1b6e55
최지원 <wawa0615@likelion.org>
2021-05-23 16:32:44 +0900

    남아공트렌드 뷰파일

--- Trends/main/views.py
+++ Trends/main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from pytrends.request import TrendReq
+import pandas as pd
+import json
 
-# Create your views here.
+def trend_sa(request):
+    pytrend = TrendReq()
+    trend_sa = pytrend.trending_searches(pn='south_africa')
+
+    trend_sa.to_csv('Trends_SA.csv', encoding='utf-8-sig')
+    df = pd.read_csv("Trends_SA.csv")
+
+    json_records = df.reset_index().to_json(orient ='records')
+    data = []
+    data = json.loads(json_records)
+    context = {'d': data}
+
+    return render(request, 'trend_sa.html', context)
