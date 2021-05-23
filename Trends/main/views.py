1c4679fb97e380f66a104c7d6955747854ec7896
jihyeon-01 <spielraum_@likelion.org>
2021-05-23 16:30:42 +0900

    한국트렌드뷰파일

--- Trends/main/views.py
+++ Trends/main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from pytrends.request import TrendReq
+import pandas as pd
+import json
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def trend_kr(request):
+    pytrend = TrendReq()
+    trend_kr = pytrend.trending_searches(pn='south_korea')
+
+    trend_kr.to_csv('Trends_kr.csv', encoding='utf-8-sig')
+    df = pd.read_csv("Trends_kr.csv")
+
+    json_records = df.reset_index().to_json(orient ='records')
+    data = []
+    data = json.loads(json_records)
+    context = {'d': data}
+
+    return render(request, 'trend_kr.html', context)
